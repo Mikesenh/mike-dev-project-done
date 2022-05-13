d84076cdc02039c2fa363a042e79aae84666fd61
Michael Tran <michaeltranperson@gmail.com>
2022-05-13 17:20:53 -0600

    120% Improvment!

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -53,21 +53,10 @@ export default function Portfolio() {
           sitelink="https://rockpaperscissors-rps-game.vercel.app/"
         />
         <Projbox
-          imagesrc="/thumbnail/starway_thumbnail.png"
-          imagealt="starway a fictional design I had on the fly"
-          codestack="N/A Only Design"
-          projectname="Just Web Designing: Starway"
-          date="4/26/2022 - 4/28/2022"
-          summary="As you may know, this site is new, so I only want the best type of work on it. I figure I add in what I currently playing around with in the design department a company specializing in space travel! it's all theory, but a fun exercise non the less."
-          figmalink="https://www.figma.com/file/ezHQDY7Kiqn19O5Stv2sUy/Project-JS%3A-StarWay?node-id=6%3A2"
-          githublink=""
-          sitelink=""
-        />
-        <Projbox
           imagesrc="/thumbnail/janadhi_thumbnail.png"
           imagealt="Janadhi & Company was a Company I worked for!"
           codestack="HTML / CSS / JavaScript / Bootstrap"
-          projectname="School Project: Janadhi & Company"
+          projectname="Website: Janadhi & Company"
           date="1/29/2021 - 3/28/2021"
           summary="I had a school assignment to update an old company website, It was a mixture of contacting the owner of the site and doing good free work! it was a whole lot of design and after that a whole lot of coding."
           figmalink="https://www.figma.com/file/h82egEsVmUnZLWjlcVmKCg/Janadhi-%26-Company-Design?node-id=0%3A1"
@@ -75,9 +64,32 @@ export default function Portfolio() {
           sitelink="https://janadhi.com/V9/index.html"
         />
         <Projbox
+          imagesrc="/thumbnail/corestrength_thumbnail.png"
+          imagealt="UX UI of Core Strength"
+          codestack="Figma / Only Design / UX / UI"
+          projectname="UX Assignment: CoreStrength"
+          date="1/29/2021 - 3/28/2021"
+          summary="I had a school assignment to update an old company website, It was a mixture of contacting the owner of the site and doing good free work! it was a whole lot of design and after that a whole lot of coding."
+          figmalink="https://www.figma.com/file/sVCFmKHJGZwEnHjeQlPmYK/Final-Prototype-Done?node-id=302%3A5"
+          githublink=""
+          sitelink=""
+        />
+        <Projbox
+          imagesrc="/thumbnail/starway_thumbnail.png"
+          imagealt="starway a fictional design I had on the fly"
+          codestack="Figma / Only Design / UX / UI"
+          projectname="Just Web Designing: Starway"
+          date="4/26/2022 - 4/28/2022"
+          summary="As you may know, this site is new, so I only want the best type of work on it. I figure I add in what I currently playing around with in the design department a company specializing in space travel! it's all theory, but a fun exercise non the less."
+          figmalink="https://www.figma.com/file/ezHQDY7Kiqn19O5Stv2sUy/Project-JS%3A-StarWay?node-id=6%3A2"
+          githublink=""
+          sitelink=""
+        />
+        
+        <Projbox
           imagesrc="/thumbnail/shepherd_thumbnail.png"
           imagealt="probate shepherd member probate attorneys"
-          codestack="N/A Only Design"
+          codestack="Figma / Only Design / UX / UI / Contract"
           projectname="Client Design: Probate Shepherd"
           date="2/29/2022 - 4/01/2022"
           summary="my first ever contract work, where I was paid to design a website working with the client to get the image perfect! That's one of my real-world professional experience so far."
